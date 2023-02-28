556c13f6166db08e250495540de7dd11a98cc641
akashkbarik <akashkbarik12@gmail.com>
2023-02-28 23:03:01 +0530

    Added: monthly payment code

--- Junit_Programs.java
+++ Junit_Programs.java
@@ -10,7 +10,10 @@ public class Junit_Programs {
         System.out.println("day is " + weekDay() + "th day of week");
 //        temperature call
         ConvertTemperature();
+//        monthly payment call
+        System.out.println("monthly payment is : " + Monthly_Payment());
     }
+
     public static class vending_machine {
         static int n, total;
         static int[] notes = {1000, 500, 100, 50, 10, 5, 2, 1};
@@ -47,21 +50,34 @@ public class Junit_Programs {
         return d0;
 
     }
-//    Temperature code
-    public static void ConvertTemperature(){
+
+    //    Temperature code
+    public static void ConvertTemperature() {
         System.out.println("this is Temperature code\n......................");
         System.out.println("enter temp in celsius or fahrenheit");
         int tem = sc.nextInt();
         char t = sc.next().charAt(0);
-        int convertor=0;
+        int convertor = 0;
         if (t == 'c' || t == 'C') {
             convertor = (tem * 9 / 5) + 32;
-        System.out.println("converted temp is " + convertor+" F ");
+            System.out.println("converted temp is " + convertor + " F ");
         } else if (t == 'f' || t == 'F') {
             convertor = (tem - 32) * 5 / 9;
-            System.out.println("converted temp is " + convertor+" C ");
+            System.out.println("converted temp is " + convertor + " C ");
         } else {
             System.out.println("Enter correct input");
         }
+    }
+
+    //    Monthly payment code
+    public static double Monthly_Payment() {
+        System.out.println("enter principal ,year and rate");
+        double p = sc.nextDouble();
+        double y = sc.nextDouble();
+        double R = sc.nextDouble();
+        double n = 12 * y;
+        double r = R / (12 * 100);
+        double payment = p * r / (1 - Math.pow((1 + r), -n));
+        return payment;
     }
 }
